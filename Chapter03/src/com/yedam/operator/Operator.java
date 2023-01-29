c9921a6d92797d6d433344c27941c9a02d4bd67d
82109 <82109@192.168.123.196>
2023-01-29 11:24:21 +0900

    Chapter03

---
+++ Chapter03/src/com/yedam/operator/Operator.java
@@ -0,0 +1,13 @@
+package com.yedam.operator;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		//증감연산자
+		int a = 0;
+		System.out.println(a++); //실행문이 종료되고 +1 주로 많이 사용
+		
+		System.out.println(++a); //1이 된 상태로 내려온 후 +1
+	}
+
+}
