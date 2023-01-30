ff442c9d9699293350b7402857f6efdc2ca0034a
82109 <82109@192.168.123.196>
2023-01-30 23:55:50 +0900

    HomeWork0130

--- HomeWork0130/src/homework/Homework.java
+++ HomeWork0130/src/homework/Homework.java
@@ -35,21 +35,58 @@ public class Homework {
 		// 1900년은 100의 배수이고 400의 배수는 아니기 때문에 윤년이 아닙니다.
 		// HiNT : 이중 IF문 사용
 		// 문제출처, 백준(https://www.acmicpc.net/) 2753번 문제
-		int year = 2022;
-		if(year%4==0 && year%100!=0) {
-			
-		}
+		System.out.println("연도를 입력하세요.>");
+		int year = Integer.parseInt(scanner.nextLine());
+		if(year%4==0 && (year%100 != 0 || year%400 == 0)){
+            System.out.println("윤년입니다.");
+        }else{
+            System.out.println("윤년이 아닙니다.");
+        }
 		
 		// 문제3) switch문을 이용하여 가위, 바위, 보 중 하나가 주어졌을 때 사용자 어떤 값을 가져야 이길 수 있는 지를 출력하도록
 		// 구현하세요.
 		// 예를 들어, 가위가 주어졌을 때 "이기기 위해선 바위를 내야합니다." 라고 출력하도록 하세요.
 		// 한 단어를 입력받을 경우 Scanner객체의 next() 함수를 사용하면 됩니다.
+		System.out.println("가위, 바위, 보!");
+		String s=scanner.nextLine();
+		switch(s) {
+		case "가위":
+			System.out.println("이기기 위해선 바위를 내야합니다.");
+			break;
+		case "바위":
+			System.out.println("이기기 위해선 보를 내야합니다.");
+			break;
+		case "보":
+			System.out.println("이기기 위해선 가위를 내야합니다.");
+			break;
+		default:
+			System.out.println("\'가위, 바위, 보\'로 입력해주세요.");
+		}
+		
 
 		// 문제4) 차례대로 m과 n을 입력받아 m단을 n번째까지 출력하도록 하세요.
 		// 예를 들어 2와 3을 입력받았을 경우 아래처럼 출력합니다.
 		// 2 X 1 = 2
 		// 2 X 2 = 4
 		// 2 X 3 = 6
+		System.out.println("m값을 입력해주세요.>");
+		int m = Integer.parseInt(scanner.nextLine());
+		System.out.println("n값을 입력해주세요.>");
+		int n = Integer.parseInt(scanner.nextLine());
+		
+		for(int i=m; i<=n; i++) {
+			for(int j=1;j>i ;j++) {
+				System.out.println(m+" x "+n+" = " + (m*n));
+			}
+			
+		}
+		//for(int i=m;i>=n;i++) {
+			//for(int j=1;j>=i;j++) {
+				
+			//	System.out.println(m+" X "+n+" = "+(m*n));
+		//	}
+			
+		//}
 
 		//문제5) 호수에서 살고 있는 얼음요정이 곱셈을 공부하기로 했다. 근데 이 요정은 너무 멍청해서 9라는 숫자 이외에는 헷갈려서 잘 쓰지 못한다고 한다. 그래서 사람과는 곱셈방식이 좀 다르다.
 		//(규칙 1)
@@ -81,6 +118,32 @@ public class Homework {
 		//1*7=9	 2*7=9	 3*7=9 	4*7=9 	5*7=9 	6*7=9 	7*7=49 	8*7=9 	9*7=63 
 		//1*8=9	 2*8=9	 3*8=9 	4*8=9 	5*8=9 	6*8=9 	7*8=9 	8*8=9 	9*8=72 
 		//1*9=9	 2*9=18	 3*9=27 4*9=36 	5*9=45 	6*9=54 	7*9=63 	8*9=72 	9*9=81
+		
+		System.out.println("n 값을 입력하세요.>");
+		int nn = Integer.parseInt(scanner.nextLine());
+		for(int i=1; i<nn; i++) {
+			for (int j=1; j<nn; j++){
+				int math = i * j; //i*j 변수 선언
+				boolean boo=true;//while문 탈출용 변수
+				while(boo) {
+					if(math % 10 ==9) {
+						math %= 10;
+						boo = false;
+					}else if (math >= 10) {
+						math /= 10;
+					}else if (math < 10) {
+						boo = false;
+					}
+				}
+				if (i%10==9 || j%10==9 || math ==9) {
+					System.out.printf("%d * %d = %d\t", j, i, i*j);
+				}else {
+					System.out.printf("%d * %d = 9\t", j, i);
+				}
+			}
+			System.out.println();
+		}
+		
 
 	}
 
