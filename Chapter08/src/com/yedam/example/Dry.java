5cf12910b44e7fdf8461671acf2d315b7795059e
admin <admin@YD05-01>
2023-02-06 17:21:33 +0900

    Chapter08

---
+++ Chapter08/src/com/yedam/example/Dry.java
@@ -0,0 +1,5 @@
+package com.yedam.example;
+
+public interface Dry {
+	public void dry();
+}
---
+++ Homework/src/homework0206/Culture.java
@@ -0,0 +1,23 @@
+package homework0206;
+
+public class Culture {
+	//필드
+	//제목, 참여감독 수, 참여배우 수, 관객수, 총점을 필드로 가진다.
+	public String title;
+	int movieDirector;
+	public int actors;
+	public int audience;
+	public int totalScore;
+	
+	//생성자
+	//제목, 참여감독 수, 참여배우 수는 생성자를 이용하여 값을 초기화한다.
+	public Culture(String title, int movieDirector, int actors) {
+		
+	}
+	
+	//메소드
+	//관객수와 총점을 누적시키는 기능
+	public void setTotalScore(int score) {
+		
+	}
+}
