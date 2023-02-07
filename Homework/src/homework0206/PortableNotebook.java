b60f666bb70c098317f83ef0ef6029ad693e20bf
82109 <82109@192.168.123.186>
2023-02-08 00:19:08 +0900

    Homework

--- Homework/src/homework0206/PortableNotebook.java
+++ Homework/src/homework0206/PortableNotebook.java
@@ -3,43 +3,64 @@ package homework0206;
 public class PortableNotebook implements Notebook, Tablet{
 	
 	//필드
-	private int currentMode;
+	private int mode;
 	String document;
-	String brower;
+	String browser;
 	String video;
 	String app;
 	
 	//생성자
 	
 	// 생성자
-	public PortableNotebook() {
-			currentMode = Notebook.NOTEBOOK_MODE;
-			System.out.println(currentMode);
+	public PortableNotebook(String documet, String browser,String video, String app) {
+			mode = Notebook.NOTEBOOK_MODE;
+			System.out.println("NOTEBOOK_MODE");
+			this.document = documet;
+			this.browser = browser;
+			this.video = video;
+			this.app =app;
+					
 		}
 	
 	//메소드
 	@Override
 	public void watchVideo() {
-		System.out.println("'영상종류'를 시청.");
+		System.out.println(video+"를 시청.");
 		
 	}
 
 	@Override
 	public void useApp() {
-		
+		if(mode==Notebook.NOTEBOOK_MODE) {
+			mode=Notebook.TABLET_MODE;
+		System.out.println(app+"를 실행.");
+	}else if(mode==Notebook.TABLET_MODE) {
+		System.out.println(app+"를 실행.");
+	}
 	}
 
 	
 	@Override
 	public void writeDocumentaion() {
-		System.out.println("'문서 프로그램'을 통해 문서를 작성.");
+		System.out.println(document+"을 통해 문서를 작성.");
 		
 	}
 
 	@Override
 	public void searchInternet() {
-		System.out.println("'인터넷 브라우저'를 통해 인터넷을 검색.");
+		System.out.println(browser+"를 통해 인터넷을 검색.");
 		
 	}
+	
+	public void changeMode() {
+		if(mode==NOTEBOOK_MODE) {
+			mode=TABLET_MODE;
+			System.out.println("TABLET_MODE");
+		}else {
+			mode=NOTEBOOK_MODE;
+			System.out.println("NOTEBOOK_MODE");
+		}
+	}
 
+	
 }
